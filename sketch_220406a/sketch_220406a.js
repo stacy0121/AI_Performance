53c66b621a1b34e760f634129a15140e9caff064
stacy012 <72933504+stacy0121@users.noreply.github.com>
2022-04-06 17:39:45 +0900

    0406

---
+++ sketch_220406a/sketch_220406a.js
@@ -0,0 +1,67 @@
+// Classifier Variable
+let classifier;
+// Model URL
+let imageModelURL = './my_model/';
+
+// Video
+let video;
+let flippedVideo;
+// To store the classification
+let label = "";
+
+// Load the model first
+function preload() {
+  //classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/asnxwwhKK/' + 'model.json');
+   classifier = ml5.imageClassifier('./libraries/model.json');
+}
+
+function setup() {
+  //createCanvas(320, 260);
+  createCanvas(windowWidth, windowHeight);
+  // Create the video
+  video = createCapture(VIDEO);
+  //video.size(320, 240);
+  video.size(windowWidth, windowHeight);
+  video.hide();
+
+  flippedVideo = ml5.flipImage(video);
+  // Start classifying
+  classifyVideo();
+  createDiv();   // 내가 한 거 
+}
+
+function draw() {
+  background(0);
+  // Draw the video
+  //image(flippedVideo, 0, 0);
+  image(flippedVideo, 0, 0, windowWidth, windowHeight);
+
+  // Draw the label
+  fill(255);
+  textSize(16);
+  textAlign(CENTER);
+  text(label, width / 2, height - 4);
+}
+
+// Get a prediction for the current video frame
+function classifyVideo() {
+  flippedVideo = ml5.flipImage(video);
+  classifier.classify(flippedVideo, gotResult);
+  flippedVideo.remove();
+
+}
+
+// When we get a result
+function gotResult(error, results) {
+  // If there is an error
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // The results are in an array ordered by confidence.
+  // console.log(results[0]);
+  label = results[0].label;
+  // Classifiy again!
+  classifyVideo();
+}
+ 
