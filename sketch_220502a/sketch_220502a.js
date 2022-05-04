6b0821367c5d230473305cdb3b81f8420c53014c
stacy012 <72933504+stacy0121@users.noreply.github.com>
2022-05-04 17:29:04 +0900

    0504

--- sketch_220502a/sketch_220502a.js
+++ sketch_220502a/sketch_220502a.js
@@ -1,15 +1,60 @@
+// 마우스 방향을 따라다니는 글씨
+// 손을 따라다니는 글씨
 var texts = "ABCDEFU";
 var chars = [];
+var directionX;   // 방향
+
+let video;
+let poseNet;
+let poses = [];
+//var phand = point.position;   // 이전 손 위치. pmouseX 역할
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(640,480);
   chars = texts.split("");   // string을 ""기준으로 char 13개로 분리
+  directionX = 1;   // 정상적 움직임
+  
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  
+  poseNet = ml5.poseNet(video);
+  poseNet.on("pose", function(results){
+    poses = results;
+  });
+  
+  video.hide();   // html 상 비디오 지우기
 }
 
+//function modelReady() {
+//  select("#status").html("Model Loaded");
+//}
 
 function draw() {
   background(170,170,170);
+  if(pmouseX<mouseX){   // 왼쪽으로 움직임
+    directionX = -1;
+  }
+  if(pmouseX>=mouseX){
+    directionX = 1;
+  }
   for(var i=0;i<chars.length;i++){
-    text(chars[i],mouseX+i*10, random(mouseY-20,mouseY+20));   // 10픽셀 거리
+    text(chars[i],mouseX+directionX*i*10, random(mouseY-5,mouseY+5));   // 10 or -10픽셀 거리
+    //text(chars[i],keypoint.position.x, keypoint.position.y);
+  }
+  
+  image(video, 0, 0, width, height);
+  
+  drawKeypoints();
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function drawKeypoints() {
+  for (let i = 0; i < poses.length; i += 1) {
+    const pose = poses[i].pose;
+    const keypoint = pose.keypoints[10];   // 오른손
+    text("hello", keypoint.position.x, keypoint.position.y);
   }
 }
