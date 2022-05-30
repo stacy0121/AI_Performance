58c21aa01cb7d651e42b7fb6c56759c71648f64d
stacy012 <72933504+stacy0121@users.noreply.github.com>
2022-05-31 01:17:29 +0900

    assignment

--- sketch_220530b/sketch_220530b.js
+++ sketch_220530b/sketch_220530b.js
@@ -1,5 +1,4 @@
-// 마우스 방향을 따라다니는 글씨
-// 손을 따라다니는 글씨
+// 손목을 따라다니는 음성인식된 글씨
 var texts = "ABCDEFU";
 var chars = [];
 var directionX;   // 방향
@@ -7,12 +6,13 @@ var directionX;   // 방향
 let video;
 let poseNet;
 let poses = [];
-//var phand = point.position;   // 이전 손 위치. pmouseX 역할
+var keypointX;
+var phand;   // 이전 손목 위치. pmouseX 역할
 let speech;
 
 function setup() {
   createCanvas(640,480);
-  //directionX = 1;   // 정상적 움직임
+  directionX = 1;   // 1은 정상적 움직임
   
   video = createCapture(VIDEO);
   video.size(width, height);
@@ -33,35 +33,33 @@ function setup() {
     if(speechRec.resultValue){
       let said = speechRec.resultString;
       console.log(said);
-      //output.html(said);
       texts = said;
       chars = texts.split("");   // string을 ""기준으로 char 13개로 분리
     }
   }
-  
   video.hide();   // html 상 비디오 지우기
 }
 
-//function modelReady() {
-//  select("#status").html("Model Loaded");
-//}
-
 function draw() {
   background(170,170,170);
-  //if(pmouseX<mouseX){   // 왼쪽으로 움직임
-  //  directionX = -1;
-  //}
-  //if(pmouseX>=mouseX){
-  //  directionX = 1;
-  //}
-  //for(var i=0;i<chars.length;i++){
-    //text(chars[i],mouseX+directionX*i*10, random(mouseY-5,mouseY+5));   // 10 or -10픽셀 거리
-    //text(chars[i],keypoint.position.x, keypoint.position.y);
-  //}
-  
   image(video, 0, 0, width, height);
   
   drawKeypoints();
+  // 손목의 이전 x좌표 전달
+  phand = keypointX;
+  // 현재 x좌표 찾기 및 방향 설정
+  for (let i = 0; i < poses.length; i ++) {
+    const pose = poses[i].pose;
+    const keypoint = pose.keypoints[10];
+    if(phand<keypoint.position.x){   // 왼쪽으로 움직임
+      directionX = -1;
+    }
+    if(phand>=keypoint.position.x){
+      directionX = 1;
+    }
+  }
+  textSize(15);
+  text("오른팔을 올리고 한글 단어를 말해보세요. 팔을 움직이는 방향에 따라 글자가 따라갑니다.", 20, 40);
 }
 
 function windowResized(){
@@ -69,12 +67,13 @@ function windowResized(){
 }
 
 function drawKeypoints() {
-  for (let i = 0; i < poses.length; i += 1) {
-    for(var j=0;j<chars.length;j++){
+  for(let i=0; i<poses.length; i++) {
+    for(var j=0; j<chars.length; j++){
       const pose = poses[i].pose;
-      const keypoint = pose.keypoints[10];   // 오른손
-      //text("hello", keypoint.position.x, keypoint.position.y);
-      text(chars[j],keypoint.position.x, keypoint.position.y);
+      const keypoint = pose.keypoints[10];     // 오른팔 손목
+      keypointX = keypoint.position.x;         // x좌표
+      const keypointY = keypoint.position.y;   // y좌표
+      text(chars[j], keypointX+directionX*j*15, random(keypointY-3, keypointY+3));
     }
   }
 }
